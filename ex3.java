8cf4cf8bb36bb43f44aefa58695b58f24c8cc61b
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-03-01 12:50:40 -0600

    ex3 finished

--- ex3.java
+++ ex3.java
@@ -20,17 +20,45 @@ public class ex3 {
     }
 
     public static void main(String[] xx) {
-        Scanner sc = new Scanner(System.in);
 
-        ex3 primera=new ex3();
-        ex3 segunda=new ex3(5,7);
-        ex3 resultado=new ex3();
-        
-        System.out.print("Ingrese el numerador: ");
-        primera.numerador = sc.nextInt();
-        System.out.print("Ingrese el denominador: ");
-        primera.denominador = sc.nextInt();
-        resultado=segunda.SumarFraccion(primera, segunda);
-        System.out.println("Resultado de la fracción: " + resultado.numerador + "/" + resultado.denominador);
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("Ingrese la cantidad de fracciones: ");
+            int n = sc.nextInt();
+            if (n > 0) {
+
+            ex3[] fracciones = new ex3[n]; // arreglo para almacenar las fracciones
+
+            // solicitar los valores de cada fracción
+            for (int i = 0; i < n; i++) {
+                System.out.println("\n-------------------------------------\n");
+                System.out.println("Ingrese los valores de la fracción " + (i+1) + ":");
+                System.out.print("Numerador: ");
+                int numerador = sc.nextInt();
+                System.out.print("Denominador: ");
+                int denominador = sc.nextInt();
+                
+                fracciones[i] = new ex3(numerador, denominador); // crear objeto fracción y almacenarlo en el arreglo
+            }
+            
+            // sumar las fracciones
+            ex3 resultado = fracciones[0];
+            for (int i = 1; i < n; i++) {
+                resultado = resultado.SumarFraccion(fracciones[i], resultado);
+            }
+            // mostrar el resultado
+            System.out.println("\n-------------------------------------\n");
+            System.out.println("El resultado es:\n");
+            System.out.print(resultado.numerador); 
+            System.out.print("\n--\n");
+            System.out.print( resultado.denominador + "\n");
+
+            
+            }else{
+                System.out.println("¡ERROR!\nNo se permiten números negativos o '0' para esta solicitud.");
+            }
+
+            break;
+        }while(true);
     }
 }
